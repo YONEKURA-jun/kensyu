256ba84cf85abca86e8212f74731792fd4fb20fe
YONEKURA-jun <abyamo7j@gmail.com>
2026-08-21 14:21:50 +0900

    外部管理フォルダに管理用ヘッダーデータを追加。プログラム開始時に作成する配列を基に外部フォルダパスを取得する形にセーブ・ロード関係機能を改修。新たなセーブが追加された際に保持している配列のサイズを変更する機能を追加。記録しているパスから本体データへアクセス出来なかった際、配列のサイズを縮小させる機能を追加。保持している管理配列を基に画面に外部フォルダのパスを表示させる機能を追加。ヘッダ更新用の関数を追加。

--- Luidas.c
+++ Luidas.c
@@ -15,6 +15,9 @@
 #define SAVE_PATH 15
 #define SAVE_SLOT 8
 #define NEW_DATA -1
+#define HEADER_SIZE 15
+#define HEADER 1
+#define TXT_PATH 16
 
 enum CHOICE {
 	LIBRARY,
@@ -68,6 +71,7 @@ char* p_gender_list[GENDER] = {
 
 static int g_person = 0;//現在のパーティー人数を格納する
 static char (*g_MemoeyCard_copy)[SAVE_PATH] = NULL;
+static int  g_save_data_count = 0;
 
 int show_start_screen(int button);//スタート画面
 void add_members_screen();//仲間追加画面
@@ -97,7 +101,8 @@ MEMBER* insartion_sort(MEMBER* base, MEMBER* key, bool(*compare_member)(MEMBER*,
 void to_member_sort(int sort_type);
 
 bool target_gender(MEMBER* base, MEMBER* key);//比較を目的に対象の値を取り出す
-bool target_job(MEMBER* base, MEMBER* key);//
+bool target_job(MEMBER* base, MEMBER* key);//上記同
+
 //入力された値に対応して、ソートの対象を取り出す為の関数を選ぶ。
 bool(*target_list(int sort_type))(MEMBER*, MEMBER*);
 
@@ -105,20 +110,21 @@ bool(*target_list(int sort_type))(MEMBER*, MEMBER*);
 void to_save_member();//現在のリストの中身を保存する
 void to_load_member();//保存されているデータを読み込んでノードを作成する
 
-int show_MemoryCard();//Memory_Cardフォルダの中身を一覧表示する
+void show_MemCard();//Memory_Cardフォルダの中身を一覧表示する
 void check_error_fnf(int choice, int max_count);//ファイル喪失時にMemory_Cardフォルダの中身からpathを削除する
 
+void make_MemCard_copy_array();//外部管理データを参照しその内容をコピーした配列を作成する関数
+void write_header();//セーブデータ管理ファイルのヘッダに、全件数を書き込む関数
 
-void make_MemoryCard_copy_array();//外部テキストデータを参照しその内容をコピーした配列を作成する関数
-int get_target_file_size(const char* target_name);//対象のファイルのサイズを取得し、その大きさを返す関数
-
-// int check_character_input(char base[]);//不正な内容のテキスト入力だった場合、規定の内容の値を返す関数
-void test_printer(int target_size);//テスト表示用検査ツール
+//プログラム内でセーブデータ管理ファイルを開く際、入力されたパスをファイルに追加し、ヘッダ部分を更新する。
+void data_save(char save_path[SAVE_PATH]);
+void grow_MemCard_copy(char save_path[SAVE_PATH]);//管理データのコピーに新しいデータを追加する関数
+void truncate_MemCard_copy(int target_num);//管理データのコピーからデータを削除する関数
 
 
 int main(void) {
 	bool end_game_flag = 0;
-	make_MemoryCard_copy_array();
+	make_MemCard_copy_array();
 
 	while (end_game_flag == 0) {
 		int n_you_choice = INPUT_FAILED;
@@ -519,11 +525,11 @@ void to_save_member() {
 	int check_one = INPUT_FAILED;
 	int check_two = INPUT_FAILED;
 	int max_title_size = SAVE_TITLE - 1;
-	int max_count = 0;
+	int max_count = g_save_data_count;
 	int temp_choice = 0;
 
 
-	max_count = show_MemoryCard();
+	show_MemCard();
 	if (max_count == 0) {
 		temp_choice = 0;
 	}
@@ -538,15 +544,8 @@ void to_save_member() {
 		}
 	}
 	if (1 <= temp_choice && temp_choice <= max_count) {
-		printf("上書セーブだな？\n");
-		if (fopen_s(&fp, "Memory_Card.txt", "rb") != 0) {
-			to_error_reaction();
-			return;
-		}
-		for (int target = 1; target < temp_choice; target++) {
-			fgets(save_path, sizeof(save_path), fp);
-		}
-		fclose(fp);
+		printf("上書セーブだな？私がやっておこう\n");
+		sprintf_s(save_path, SAVE_PATH, "%s", g_MemoeyCard_copy[temp_choice-1]);
 	}
 	else if (temp_choice < 1 || max_count < temp_choice) {
 		printf("セーブデータの名前を入力してくれ\n");
@@ -556,14 +555,9 @@ void to_save_member() {
 			printf("長すぎる、ひらがなで%d文字、英語か数字なら%d文字だ\n", max_title_size / 2, max_title_size);
 			return;
 		}
-
-		if (fopen_s(&fp, "Memory_Card.txt", "ab") != 0) {
-			printf("メモリーカードへの書き込みに失敗した\n");
-			return;
-		}
 		sprintf_s(save_path, sizeof(save_path), "%s.txt", save_slot);
-		fprintf(fp, "%s\n", save_path);
-		fclose(fp);
+		data_save(save_path);
+		grow_MemCard_copy(save_path);
 	}
 
 	if (fopen_s(&fp, save_path, "wb") != 0) {
@@ -580,34 +574,21 @@ void to_save_member() {
 }
 
 void to_load_member() {
-	char save_path[SAVE_PATH] = { 0 };
 	int count = 0;
-	int max_count;
+	int max_count = g_save_data_count;
 	FILE* fp;
 
-	max_count = show_MemoryCard();
+	show_MemCard();
 	if (max_count == 0) {
 		return;
 	}
-
 	printf("全部で %d 組のパーティが記録されている、何番目のパーティを連れてくる？\n", max_count);
 	count = very_safety_input(1, max_count);
 	if (count == INPUT_FAILED) {
 		to_error_reaction();
 		return;
 	}
-	if (fopen_s(&fp, "Memory_Card.txt", "rb") != 0) {
-		to_error_reaction();
-		return;
-	}
-	for (int target = 0; target < count; target++) {
-		fgets(save_path, sizeof(save_path), fp);
-	}
-	save_path[strcspn(save_path, "\r\n")] = '\0';
-	fclose(fp);
-
-
-	if (fopen_s(&fp, save_path, "rb") != 0) {
+	if (fopen_s(&fp, g_MemoeyCard_copy[count-1], "rb") != 0) {
 		check_error_fnf(count, max_count);
 		return;
 	}
@@ -706,11 +687,13 @@ int very_safety_input(int lowest, int highest) {
 
 void check_error_fnf(int choice, int max_count) {
 	int temp_check = 0;
-	char save_path[SAVE_PATH] = { 0 };
+	char save_path[TXT_PATH] = { 0 };
 	FILE* base = NULL;
 	FILE* copy = NULL;
-
+	char emoty_header[] = "a0a0a0a0a0a0a0\n";
 	temp_check = errno;
+
+
 	if (temp_check != ENOENT) {
 		return;
 	}
@@ -720,11 +703,15 @@ void check_error_fnf(int choice, int max_count) {
 			fopen_s(&copy, "Copy_Card.txt", "wb") != 0) {
 			if (base != NULL) { fclose(base); }
 			if (copy != NULL) { fclose(copy); }
-			to_error_reaction();
+			printf("あっ…ファイルの修正に失敗した\n");
 			return;
 		}
+		
+		fwrite(emoty_header, sizeof(char), HEADER_SIZE, copy);
+		fseek(base, 15, SEEK_SET);
+
 		for (int target = 0; target < max_count; target++) {
-			fgets(save_path, sizeof(save_path), base);
+			fgets(save_path, TXT_PATH, base);
 			if (target == choice - 1) {
 				printf("%sは消えてしまった\n", save_path);
 			}
@@ -734,74 +721,56 @@ void check_error_fnf(int choice, int max_count) {
 	fclose(base);
 	fclose(copy);
 	remove("Memory_Card.txt");
+
 	if (rename("Copy_Card.txt", "Memory_Card.txt") != 0) {
 		perror("何かがおかしい、冒険の書は全て消えてしまった");
 	}
+	else {
+		write_header();
+		truncate_MemCard_copy(choice-1);
+	}
 }
 
-
-int show_MemoryCard() {
-	FILE* fp;
-	char temp_path[SAVE_PATH] = { 0 };
-	int temp_count = 0;
-
-	if (fopen_s(&fp, "Memory_Card.txt", "rb") != 0) {
-		to_error_reaction();
-		return;
+void show_MemCard() {
+	for (int i = 0; i < g_save_data_count; i++) {
+		printf("%d:%s\n", i + 1, g_MemoeyCard_copy[i]);
 	}
-	printf("\n");
-	while (fgets(temp_path, sizeof(temp_path), fp) != NULL) {
-		temp_count++;
-		temp_path[strcspn(temp_path, "\r\n")] = '\0';
-		printf("%d . %s\n", temp_count, temp_path);
-	}
-	if (temp_count == 0) {
+	if (g_save_data_count == 0) {
 		printf("セーブデータは無いな\n");
 	}
-	rewind(fp);
-	fclose(fp);
-	return(temp_count);
 }
 
-
-void make_MemoryCard_copy_array() {
-	long target_size = 0;
-	long haw_many = 0;
-	char temp_path[SAVE_PATH] = { 0 };
+void make_MemCard_copy_array() {
+	long how_many = 0;
+	char temp_path[TXT_PATH] = { 0 };
 	FILE* fp;
 	int src_data_counter = 0;
 	int temp;
-
-
-	target_size = get_target_file_size("Memory_Card.txt");
-	if (target_size == -1) {
-		printf("サイズの読取に失敗した、戻るぞ\n");
-		return;
-	}
+	int checker = 0;
 
 	if (fopen_s(&fp, "Memory_Card.txt", "rb") != 0) {
 		to_error_reaction();
 		return;
 	}
-
-	g_MemoeyCard_copy = calloc((target_size / SAVE_PATH)+1, SAVE_PATH);
-
-	while (fgets(temp_path, sizeof(temp_path), fp) != NULL) {
+	fscanf_s(fp, "%ld\n", &how_many);
+	g_MemoeyCard_copy = calloc(how_many, SAVE_PATH);
+	
+	while (fgets(temp_path, TXT_PATH, fp) != NULL) {
 		temp_path[strcspn(temp_path, "\r\n")] = '\0';
-		sprintf_s(g_MemoeyCard_copy[src_data_counter], SAVE_PATH, temp_path);
+		sprintf_s(g_MemoeyCard_copy[src_data_counter], SAVE_PATH, "%-14.14s", temp_path);
 		src_data_counter++;
 	}
-	printf("src_data_counter = %d\n", src_data_counter);
 	fclose(fp);
-	test_printer(src_data_counter);
+
+	g_save_data_count = how_many;
 }
 
-int get_target_file_size(const char* target_name) {
+void write_header() {
 	FILE* fp;
 	int checker = 0;
-	int file_size = 0;
+	long long file_size = 0;
 
-	if (fopen_s(&fp, target_name, "rb") != 0) {
+	if (fopen_s(&fp, "Memory_Card.txt", "rb+") != 0) {
 		printf("対象ファイルを開くのに失敗した");
 		return;
 	}
@@ -809,37 +778,74 @@ int get_target_file_size(const char* target_name) {
 	if (checker != 0) {
 		file_size = -1;
 	}
-
 	if (file_size != -1) {
 		file_size = ftell(fp);
 	}
+	checker = fseek(fp, 0, SEEK_SET);
+	if (checker != 0) {
+		file_size = -1;
+	}
+	if (file_size != -1) {
+		fprintf_s(fp, "%14lld\n", (file_size / SAVE_PATH) - HEADER);
+	}
 	fclose(fp);
-	return(file_size);
 }
 
-void test_printer(int count) {
-		printf("count = %d\n\n", count);
+void data_save(char save_path[SAVE_PATH]) {
+	FILE* fp;
+	long long temp = 0;
+	char emoty_header[] = "a0a0a0a0a0a0a0\n";
 
-		for (int i = 0; i < count; i++)
-		{
-			printf("[%d]\n", i);
-			printf("len = %zu\n", strlen(g_MemoeyCard_copy[i]));
-			printf("data = [%s]\n", g_MemoeyCard_copy[i]);
-		}
+	if (fopen_s(&fp, "Memory_Card.txt", "rb+") != 0) {
+		printf("対象ファイルを開くのに失敗した");
+		return;
 	}
-
-/*int check_character_input(char base[]) {//一旦封印、作業再開は後
-	char temp[5];
-
-	int target = 1;
-	while (temp !='\0') {//最初の４バイトで中身がどのサイズかを判定　　　　しないと行けない。
-		temp[0] = base[target];
-		isalnum(base[target]);
-
+	if (fscanf_s(fp, "%014lld\n", &temp) != 1) {
+		fwrite(emoty_header, sizeof(char), HEADER_SIZE, fp);
 	}
+	fseek(fp, 0, SEEK_END);
+	fprintf(fp, "%-14.14s\n", save_path);
+	fclose(fp);
 
-}*/
+	write_header();
+}
 
+void grow_MemCard_copy(char save_path[SAVE_PATH]) {
+	char* temp;
+	int max_count = g_save_data_count;
+
+	temp = realloc(g_MemoeyCard_copy, (max_count + 1) * SAVE_PATH);
+	if (temp == NULL) {
+		printf("MemoeyCard_copyの拡張に失敗した");
+		return;
+	}
+	g_MemoeyCard_copy = temp;
+	sprintf_s(g_MemoeyCard_copy + (max_count), SAVE_PATH, "%-14.14s", save_path);
+	g_save_data_count++;
+}
+
+void truncate_MemCard_copy(int target_num) {
+	char* temp;
+	int max_count = g_save_data_count;
+	int move_range = 0;
+
+	if (max_count <= 1) {
+		free(g_MemoeyCard_copy);
+		g_MemoeyCard_copy = NULL;
+		g_save_data_count = 0;
+		return;
+	}
+	move_range = (max_count-1) - target_num;
+	memmove(g_MemoeyCard_copy + (target_num), g_MemoeyCard_copy + (target_num + 1), move_range * SAVE_PATH);
+
+	temp = realloc(g_MemoeyCard_copy, (max_count - 1) * SAVE_PATH);
+	if (temp == NULL) {
+		printf("MemoeyCard_copyの縮小に失敗した");
+		return;
+	}
+	g_MemoeyCard_copy = temp;
+	g_save_data_count--;
+}
 /*
 void temp_bogo_sort(int target) {
 	MEMBER* temp;
